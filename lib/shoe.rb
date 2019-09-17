cfa3419395d6d99989a8d0c7b500b699bfc58ceb
Marco McNulty <mmc90@hotmail.co.uk>
2019-09-17 20:53:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  brands = []
 
   def initialize(brand)
     @brand = brand
+    brands << brand
   end
 
   def cobble
